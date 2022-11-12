9cf8d20fea605395cae3613552dd0ca744c9729b
RobertBostrom4 <Robertbostrom4@gmail.com>
2022-11-12 14:50:39 -0700

    Add solution to palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    let lowerCase = word.toLowerCase();
+    let onlyChars = lowerCase.match(/[a-z]/g);
+    let justTheWord = "";
+    for (i = 0; i < onlyChars.length; i++) {
+        justTheWord += onlyChars[i];
+    }
+    let palindrome = "";
+    for (i = justTheWord.length - 1; i >= 0; i--) {
+        palindrome += justTheWord[i];
+    }
+
+    return palindrome === justTheWord;
 
 };
 
