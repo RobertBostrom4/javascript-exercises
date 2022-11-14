4f41fdbc75beef5075e04639c961bf9e1cc58e24
RobertBostrom4 <Robertbostrom4@gmail.com>
2022-11-14 11:46:14 -0700

    Add solution to exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,44 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    let sequence = [];
+    let parsedString = parseInt(a);
+
+    if (parsedString > 0) {
+
+        for (i = 0; i < parsedString; i++) {
+
+
+            for (j = 1; j < parsedString; j++) {
+
+
+                let firstNumber = sequence[i - 1];
+                let secondNumber = sequence[i - 2];
+
+                if (firstNumber == undefined && secondNumber == undefined) {
+                    sequence[i] = j;
+                    break;
+                }
+                if (firstNumber == undefined) {
+                    firstNumber = 0;
+                }
+
+                if (secondNumber == undefined) {
+                    secondNumber = 0;
+                }
+
+
+                sequence[i] = (firstNumber + secondNumber);
+
+            }
+
+        }
+
+        if (sequence[parsedString - 1] == undefined) {
+            return parsedString;
+        }
+
+        return sequence[parsedString - 1];
+    }
+    return "OOPS";
 
 };
 
